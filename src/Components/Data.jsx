d915bb8be008d22979354d43b884cfa547913582
runwezt <amarikwat@gmail.com>
2025-05-19 16:12:41 +0100

    commit

--- src/Components/Data.jsx
+++ src/Components/Data.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import MainData from './MainData';
 
 function Data() {
@@ -12,8 +12,57 @@ function Data() {
 
   // Optional API request format
   const jsonData = maindata.toJSON();
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    // Trigger loading on first render
+    const loadPay = () => {
+      setTimeout(() => {
+        setLoading(true);
+      }, 2000);
+    };
+    loadPay();
+  }, []);
+
+
+
+  if (!loading) {
+    return <div className="loading">Wait... page is loading.</div>;
+  }
+
+  
+
+
+  const [formdata, setFormdata] = useState([])
+
+  const nameinput = useRef(null)
+  const emailinput = useRef(null)
+  const passinput = useRef(null)
+
+  const submitForm = (e) =>{
+    e.preventDefault();
+
+    const name = nameinput.current.value.trim()
+    const email = emailinput.current.value.trim()
+    const passW = passinput.current.value.trim()
+
+     if(!name || !email || !passW){
+       setFormdata(prev =>[...prev, `plesea fill the above form`])
+     } else{
+       setFormdata(prev =>[...prev, `Name: ${name} Email: ${email} Password: ${passW}`])
+
+       nameinput.current.value =''
+       emailinput.current.value =''
+       passinput.current.value =''
+     }
+  }
+  
+
+
+
 
   return (
+    <>
     <div style={{ padding: '20px' }}>
       <h2>User Data Preview</h2>
       <p><strong>Full Name:</strong> {maindata.getFullName()}</p>
@@ -26,6 +75,31 @@ function Data() {
       <h4>API Ready Format:</h4>
       <pre>{JSON.stringify(jsonData, null, 2)}</pre>
     </div>
+
+     
+    <div>
+      <form action="">
+        <input type="text" placeholder='fill name.....' required ref={nameinput} />
+        <input type="email"  placeholder='enter email......' required ref={emailinput}/>
+        <input type="password" placeholder='enter password....' required  ref={passinput} />
+
+        <button onClick={submitForm} type="submit">Login</button>
+      </form>
+
+
+      <div>
+        {
+          formdata.map((item,index)=>(
+            <div className='card' key={index.id}> {item} </div>
+
+          ))
+        }
+      </div>
+    </div>
+
+
+
+    </>
   );
 }
 
