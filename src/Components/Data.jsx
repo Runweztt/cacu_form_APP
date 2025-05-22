4fa8903144566d0460ba3fa43e52ac5e047a9d6e
runwezt <amarikwat@gmail.com>
2025-05-22 01:18:28 +0100

    commit

--- src/Components/Data.jsx
+++ src/Components/Data.jsx
@@ -17,7 +17,7 @@ function Data() {
 
   // Optional API request format
   const jsonData = maindata.toJSON();
- 
+
 
 
 
@@ -27,88 +27,163 @@ function Data() {
   const emailinput = useRef(null)
   const passinput = useRef(null)
 
-  const submitForm = (e) =>{
+  const submitForm = (e) => {
     e.preventDefault();
 
     const name = nameinput.current.value.trim()
     const email = emailinput.current.value.trim()
     const passW = passinput.current.value.trim()
 
-     if(!name || !email || !passW){
-       setFormdata(prev =>[...prev, `plesea fill the above form`])
-     } else{
-       setFormdata(prev =>[...prev, `Name: ${name} Email: ${email} Password: ${passW}`])
+    if (!name || !email || !passW) {
+      setFormdata(prev => [...prev, `plesea fill the above form`])
+    } else {
+      setFormdata(prev => [...prev, `Name: ${name} Email: ${email} Password: ${passW}`])
 
-       nameinput.current.value =''
-       emailinput.current.value =''
-       passinput.current.value =''
-     }
+      nameinput.current.value = ''
+      emailinput.current.value = ''
+      passinput.current.value = ''
+    }
   }
-  
 
 
+  const [count, setcount] = useState(0)
 
+
+  const increment = () => {
+    setcount(count + 1)
+  }
+
+  const drecrement = () => {
+    setcount(count - 1)
+  }
+
+  const reset = () => {
+    setcount(0)
+  }
+  const [name, setName] = useState('')
+
+  const handleChange = (e) => {
+    setName(e.target.value)
+  }
+
+  const [payment, setPayment] = useState('')
+
+  const handlepayment = (e) => {
+    setPayment(e.target.value)
+  }
 
   return (
     <>
-    <div style={{ padding: '20px' }}>
-      <h2>User Data Preview</h2>
-      <p><strong>Full Name:</strong> {maindata.getFullName()}</p>
-      <p><strong>Email:</strong> {maindata.email}</p>
-      <p><strong>Valid Email:</strong> {maindata.isEmailValid() ? 'Yes' : 'No'}</p>
-      <p><strong>Address:</strong> {maindata.address}</p>
-      <p><strong>Password Masked:</strong> {maindata.hidePassword()}</p>
-      <p><strong>Strong Password:</strong> {maindata.isStrongPassword() ? 'Yes' : 'No'}</p>
 
-      <h4>API Ready Format:</h4>
-      <pre>{JSON.stringify(jsonData, null, 2)}</pre>
-    </div>
+      <div>
 
-     
-    <div>
-      <form action="">
-        <input type="text" placeholder='fill name.....' required ref={nameinput} />
-        <input type="email"  placeholder='enter email......' required ref={emailinput}/>
-        <input type="password" placeholder='enter password....' required  ref={passinput} />
+        <h1>payment option</h1>
 
-        <button onClick={submitForm} type="submit">Login</button>
-      </form>
+        <select value={payment} onChange={handlepayment}>
+
+          <option value="select option"> sekect option </option>
+          <option value="cash">Cash</option>
+          <option value="visa">visa</option>
+          <option value="master card"> master card</option>
+        </select>
+
+        <h1>payment: {payment}</h1>
+
+
+
+
+        <input type="text" value={name} onChange={handleChange} />
+        <h1>name: {name}</h1>
+
+        <h1>Counter</h1>
+        <p>count {count}</p>
+        <button onClick={increment}>Increment</button>
+        <button onClick={drecrement}>Decrement</button>
+        <button onClick={reset}>Reset</button>
+
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      <div style={{ padding: '20px' }}>
+        <h2>User Data Preview</h2>
+        <p><strong>Full Name:</strong> {maindata.getFullName()}</p>
+        <p><strong>Email:</strong> {maindata.email}</p>
+        <p><strong>Valid Email:</strong> {maindata.isEmailValid() ? 'Yes' : 'No'}</p>
+        <p><strong>Address:</strong> {maindata.address}</p>
+        <p><strong>Password Masked:</strong> {maindata.hidePassword()}</p>
+        <p><strong>Strong Password:</strong> {maindata.isStrongPassword() ? 'Yes' : 'No'}</p>
+
+        <h4>API Ready Format:</h4>
+        <pre>{JSON.stringify(jsonData, null, 2)}</pre>
+      </div>
 
 
       <div>
-        {
-          formdata.map((item,index)=>(
-            <div className='card' key={index.id}> {item} </div>
+        <form action="">
+          <input type="text" placeholder='fill name.....' required ref={nameinput} />
+          <input type="email" placeholder='enter email......' required ref={emailinput} />
+          <input type="password" placeholder='enter password....' required ref={passinput} />
 
-          ))
-        }
-      </div>
-    </div>
+          <button onClick={submitForm} type="submit">Login</button>
+        </form>
 
-     <div className='cardD'>
-       <div className='mycard'>
-        <img src={lonRest10} alt="rest man" />
-        <h2> cheta Emmanuel</h2>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing enam illum perferendis, id aliquid ratione rerum? Natus, repellendus?</p>
 
-      </div>
-       <div className='mycard'>
-        <img src={lonRest10} alt="rest man" />
-        <h2> cheta Emmanuel</h2>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing enam illum perferendis, id aliquid ratione rerum? Natus, repellendus?</p>
+        <div>
+          {
+            formdata.map((item, index) => (
+              <div className='card' key={index.id}> {item} </div>
 
-      </div>
-       <div className='mycard'>
-        <img src={lonRest10} alt="rest man" />
-        <h2> cheta Emmanuel</h2>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing enam illum perferendis, id aliquid ratione rerum? Natus, repellendus?</p>
-
+            ))
+          }
+        </div>
       </div>
 
+      <div className='cardD'>
+        <div className='mycard'>
+          <img src={lonRest10} alt="rest man" />
+          <h2> cheta Emmanuel</h2>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing enam illum perferendis, id aliquid ratione rerum? Natus, repellendus?</p>
+
+        </div>
+        <div className='mycard'>
+          <img src={lonRest10} alt="rest man" />
+          <h2> cheta Emmanuel</h2>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing enam illum perferendis, id aliquid ratione rerum? Natus, repellendus?</p>
+
+        </div>
+        <div className='mycard'>
+          <img src={lonRest10} alt="rest man" />
+          <h2> cheta Emmanuel</h2>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing enam illum perferendis, id aliquid ratione rerum? Natus, repellendus?</p>
+
+        </div>
+
+      </div>
 
 
-     </div>
-    
+
+
 
     </>
   );
