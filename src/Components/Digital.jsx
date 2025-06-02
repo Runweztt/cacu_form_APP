1535d052f9848429ed6c6596589f9627acc17b55
runwezt <amarikwat@gmail.com>
2025-06-02 23:26:13 +0100

    commit

--- src/Components/Digital.jsx
+++ src/Components/Digital.jsx
@@ -2,10 +2,38 @@ import React, { useEffect, useRef, useState } from 'react';
 import './Ditgial.css'
 
 function Digital() {
+
+    const [time,setTime] = useState(new Date())
+   
+
+    useEffect (()=>{
+        const intervalId = setInterval(()=>{
+            setTime(new Date())
+        },1000)
+
+        return()=>{
+            clearTimeout(intervalId)
+        }
+    },[])
+
+
+    const FormatTime =()=>{
+        let hours =time.getHours()
+        const minutes = time.getMinutes()
+        const seconds = time.getSeconds()
+        const meridiem = hours >= 12 ? 'pm':'am'
+
+        hours = hours % 12 || 12;
+
+        return `${hours}:${minutes} :${seconds} :${meridiem}`
+    }
+
+
+
   return (
     <div className='clock_container'>
         <div clock>
-          <span>00:00:00</span>
+          <span>{FormatTime()}</span>
         </div>
        
     </div>
